f012f392342d076e094ad37e62933718077a3d98
MartaGBayona <marta.g.bayona@outlook.es>
2024-03-08 21:23:53 +0100

    feat: create middleware isSuperAdmin

---
+++ src/middlewares/isSuperAdmin.js
@@ -0,0 +1,14 @@
+export const isSuperAdmin = (req, res, next,) => {
+    try {
+        if(req.tokenData.name !== "super_admin") {
+            return res.status(401).json({
+                success: false,
+                message: "UNATHORIZED"
+            })
+        }
+
+        next()
+    } catch (error){
+        handleError(res, "You dont have permisions", 500)
+    }
+}
